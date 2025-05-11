b9c7ebec98678f4f0a26b04426dd23ff4f1fb0df
Asad Hussain <asadhussain2408@gmail.com>
2025-05-11 20:03:11 +0530

    Update stack_post.c

--- Stack/stack_post.c
+++ Stack/stack_post.c
@@ -1,9 +1,1 @@
-#include<stdio.h>
-#include<string.h>
-#define MAX 100
-int top=-1;char stack[MAX],arr[MAX];
-void push(char ch)
-{
-    stack[++top]=ch;
-    if()
-}
+will be adding the stack postfix soon
